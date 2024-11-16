d441454e34bfd4ff64b4255b430aac09ba24d161
Radosław Wawrzyk <radekwawrzyk@gmail.com>
2024-11-16 15:44:03 +0100

    Chat - Implement logic and styles for chat conversation and its messages

--- src/components/AppChatConversationArea/AppChatConversationArea.vue
+++ src/components/AppChatConversationArea/AppChatConversationArea.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>xd</div>
+  <ul class="app-chat-conversation">
+    <li class="app-chat-conversation__entry" v-for="message in conversation" :key="message.id">
+      <app-chat-message-entry
+        :message="message"
+        :is-made-by-author="isMessageMadeByAuthor(message)"
+      />
+    </li>
+  </ul>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppChatMessageEntry from '@/components/AppChatMessageEntry/AppChatMessageEntry.vue'
 
-<style scoped></style>
+import { useChatStore } from '@/stores/chat'
+import type { ChatMessage } from '@/types/User'
+import { computed } from 'vue'
+
+const chatStore = useChatStore()
+
+const conversation = computed(() => chatStore.messages)
+
+const isMessageMadeByAuthor = (message: ChatMessage) => {
+  return chatStore.isMessageFromCurrentUser(message)
+}
+</script>
+
+<style scoped lang="sass" src="./AppChatConversationArea.scss" />
--- src/components/AppChatMessageEntry/AppChatMessageEntry.vue
+++ src/components/AppChatMessageEntry/AppChatMessageEntry.vue
@@ -1,7 +1,36 @@
 <template>
-  <div></div>
+  <div class="app-chat-entry" :class="{ 'app-chat-entry--author': isMadeByAuthor }">
+    <div class="app-chat-entry__avatar">
+      <img
+        :src="message.from.thumbnail"
+        :alt="message.from.firstName"
+        class="app-chat-entry__avatar-image"
+      />
+    </div>
+
+    <div
+      class="app-chat-entry__content"
+      :class="{ 'app-chat-entry__content--author': isMadeByAuthor }"
+    >
+      {{ message.message }} by {{ message.from.firstName }}
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ChatMessage } from '@/types/User'
+import type { PropType } from 'vue'
 
-<style scoped></style>
+defineProps({
+  message: {
+    type: Object as PropType<ChatMessage>,
+    default: () => {},
+  },
+  isMadeByAuthor: {
+    type: Boolean,
+    default: () => false,
+  },
+})
+</script>
+
+<style scoped lang="scss" src="./AppChatMessageEntry.scss" />
